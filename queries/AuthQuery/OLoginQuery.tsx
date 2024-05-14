9be4eb22f6a7efeb06d54e8c7329459150ad529e
sakagawakyohei <lamgiabao1039@gmail.com>
2024-05-14 23:21:33 +0700

    test

--- queries/AuthQuery/OLoginQuery.tsx
+++ queries/AuthQuery/OLoginQuery.tsx
@@ -4,5 +4,8 @@ import { TypeSupabaseClient } from "@/utils/supabase";
 export function OLoginQuery(client: TypeSupabaseClient) {
   return client.auth.signInWithOAuth({
     provider: "google",
+    options: {
+      redirectTo: `https://manga-one-orpin.vercel.app/`,
+    },
   });
 }
