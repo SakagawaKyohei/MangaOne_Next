270e754868298c1e9683b3b492f2ea6accd96e49
sakagawakyohei <lamgiabao1039@gmail.com>
2024-05-14 21:25:25 +0700

    fix

--- hooks/comment/useCommentMangaAll.tsx
+++ hooks/comment/useCommentMangaAll.tsx
@@ -2,7 +2,6 @@ import { GetMangaByID } from "@/queries/GetMangaByID";
 import { useQuery } from "@tanstack/react-query";
 import useSupabase from "../useSupabase";
 import { GetAllMangaList } from "@/queries/GetMangaList/GetAllMangaList";
-import { QueryMessage } from "@/queries/message/QueryMessage";
 import { QueryCommentManga } from "@/queries/Comment/QueryCommentManga";
 import { QueryCommentMangaAll } from "@/queries/Comment/QueryCommentMangaAll";
 
